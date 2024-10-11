0b6033255d9952543b8dffabffefd3df1d0de642
silasevan <silasevan@gmail.com>
2024-10-11 12:40:47 +0100

    tyscript

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,17 @@
-# Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float
+#!/usr/bin/env python3
+"""
+This module defines a function that adds two floating-point numbers and returns their sum.
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers and returns their sum as a float.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
